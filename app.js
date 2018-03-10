7dd0357f8210b5406716fac2c47a2d7a2a6788ab
hcz-123 <haochunzhe@yoju360.com>
2018-03-10 15:22:41 +0800

    ceshi

---
+++ app.js
@@ -0,0 +1,22 @@
+import Koa from 'koa';
+import router from 'koa-simple-router';
+import initController from './controller/initController';
+import render from 'koa-swig';
+import co from 'co';
+import serve from 'koa-static';
+import Config from './config/config';
+const app = new Koa();
+
+initController.init(app,router);
+// response 
+
+app.context.render = co.wrap(render({
+  root:Config.get('viewDir'),
+  autoescape: true,
+  cache: 'memory',
+  ext: 'html'
+}));
+
+app.use(serve(Config.get('staticDir')));
+ 
+app.listen(Config.get('port'));
